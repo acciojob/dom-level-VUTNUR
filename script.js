94ba6569d94fcc984382c2208512549e24086251
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-24 09:18:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",function(){
+    let element=document.getElementById("level")
+    let val=0;
+    while(element!=document.documentElement){
+        val++;
+        element=element.parentNode;
+    }
+    alert("The level of the element is:"+val)
+})
